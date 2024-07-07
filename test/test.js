a5ee17f1b286b653bbe93d8251756e3b8d3deb54
Kanza Jovita <kanzajovita@gmail.com>
2024-07-07 14:24:50 +0300

    second pass 3 by 3 js

--- test/test.js
+++ test/test.js
@@ -5,6 +5,12 @@ describe('multiply', function(){
     it('mulitiplying 1 by 1 and should be equal to 1',function(){
         assert.equal(multiply(1, 1), 1);
     })
+    it('mulitiplying 3 by 3 and should be equal to 9',function(){
+        assert.equal(multiply(3, 3), 9);
+    })
+   
+   
+   
 
 })
 
